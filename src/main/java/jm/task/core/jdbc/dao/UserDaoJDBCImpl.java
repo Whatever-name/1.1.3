1f086268b0a72673baf4fb12e1af8b04ff001701
Anatoliy <honey_sunday@mail.ru>
2022-01-20 21:53:36 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,12 +9,13 @@ import java.sql.*;
 import java.util.*;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection;
     public UserDaoJDBCImpl() {
-
+        connection = Util.getConnection();
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.execute("CREATE TABLE IF NOT EXISTS USERS(ID INTEGER NOT NULL AUTO_INCREMENT" +
                     ", FIRSTNAME VARCHAR(255), LASTNAME VARCHAR(255)," +
                     "AGE INTEGER(3), PRIMARY KEY (ID))");
@@ -25,7 +26,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
             statement.execute("DROP TABLE IF EXISTS USERS");
 
         } catch (SQLException throwables) {
@@ -34,12 +35,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getConnection(); PreparedStatement statement =
-                connection.prepareStatement("INSERT INTO USERS (FIRSTNAME, LASTNAME, AGE) VALUES(?,?,?);")) {
-            statement.setString(1, name);
-            statement.setString(2, lastName);
-            statement.setByte(3, age);
-            statement.executeUpdate();
+        try ( PreparedStatement preparedStatement = connection.prepareStatement
+                ("INSERT INTO USERS (FIRSTNAME, LASTNAME, AGE) VALUES(?,?,?);")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
             System.out.println("User с именем " + name + " добавлен в базу данных");
 
         } catch (SQLException throwables) {
@@ -48,10 +49,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection(); PreparedStatement statement =
-                connection.prepareStatement("DELETE FROM USERS WHERE ID = ?")) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
+        try (PreparedStatement preparedStatement = connection.prepareStatement
+                ("DELETE FROM USERS WHERE ID = ?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
